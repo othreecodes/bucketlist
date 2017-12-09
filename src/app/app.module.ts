612c564b8b58c21fbea9d757dee5f41672eab725
Obi Uchenna David <daviduchenna@outlook.com>
2017-12-09 09:36:40 +0100

    add animatioins and functionality to remove goals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
@@ -16,7 +18,9 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, transition, animate, keyframes, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    
+        trigger('goals', [
+          transition('* => *', [
+    
+            query(':enter', style({ opacity: 0 }), {optional: true}),
+    
+            query(':enter', stagger('300ms', [
+              animate('.6s ease-in', keyframes([
+                style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+                style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+                style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+              ]))]), {optional: true})
+          ])
+        ])
+    
+      ]
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number;
+  btnText = 'Add an Item';
+  goalText = 'My first life goal';
+  goals= ['I wanna eat', 'I want food'];
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length;
   }
 
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+
+  }
+
+  removeItem(index) {
+    this.goals.splice(index, 1);
+  }
 }
